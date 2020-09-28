92b4713a6f0ba46a2ec4cc847d5beb8c5366abd0
zhangming <13419542675@163.com>
2020-09-28 18:47:35 +0800

    ``

--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -6,7 +6,7 @@
     <div class="btn-box" id="areaDiv">
         <span class="saveBtn" @click='saveProject'>Save risk weight scheme</span>
         <!-- 滑动条 -->
-        <div style="margin: 0 10px;">
+        <!-- <div style="margin: 0 10px;">
             <c-progress class="c-progress"
             :percent="7"
             :show-slider="true" 
